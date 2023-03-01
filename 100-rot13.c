e41855b85bd570c6f0c557775d3d1325e38686c2
tmuchui <102290030+tmuchui@users.noreply.github.com>
2023-03-01 17:56:56 +0300

    Create 100-rot13.c

---
+++ 100-rot13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char *rot13(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+            str[i] += 13;
+        else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+            str[i] -= 13;
+        i++;
+    }
+    return (str);
+}
